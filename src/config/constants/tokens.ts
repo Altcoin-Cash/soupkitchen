abdf200a9b21016601bef783a73e4134e5017efb
Altcoin-Cash <altcoincash@protonmail.com>
2021-06-24 00:11:35 +0200

    cleanup tokens

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -31,7 +31,76 @@ const tokens = {
     decimals: 18,
     projectLink: 'https://soup.finance/',
   },
- 
+  wbnb: {
+    symbol: 'wBNB',
+    address: {
+      56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+      97: '0xae13d989dac2f0debff460ac112a837c89baa7cd',
+    },
+    decimals: 18,
+    projectLink: 'https://soup.finance/',
+  },
+  syrup: {
+    symbol: 'SYRUP',
+    address: {
+      56: '0x009cF7bC57584b7998236eff51b98A168DceA9B0',
+      97: '0xfE1e507CeB712BDe086f3579d2c03248b2dB77f9',
+    },
+    decimals: 18,
+    projectLink: 'https://soup.finance/',
+  },
+  busd: {
+    symbol: 'BUSD',
+    address: {
+      56: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
+      97: '',
+    },
+    decimals: 18,
+    projectLink: 'https://www.paxos.com/busd/',
+  },
+  eth: {
+    symbol: 'ETH',
+    address: {
+      56: '0x2170Ed0880ac9A755fd29B2688956BD959F933F8',
+      97: '',
+    },
+    decimals: 18,
+    projectLink: 'https://ethereum.org/en/',
+  },
+  beth: {
+    symbol: 'BETH',
+    address: {
+      56: '0x250632378E573c6Be1AC2f97Fcdf00515d0Aa91B',
+      97: '',
+    },
+  usdt: {
+    symbol: 'USDT',
+    address: {
+      56: '0x55d398326f99059fF775485246999027B3197955',
+      97: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
+    },
+    decimals: 18,
+    projectLink: 'https://tether.to/',
+  },
+  bake: {
+    symbol: 'BAKE',
+    address: {
+      56: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
+      97: '',
+    },
+    decimals: 18,
+    projectLink: 'https://www.bakeryswap.org/',
+  },
+  doge: {
+    symbol: 'DOGE',
+    address: {
+      56: '0xbA2aE424d960c26247Dd6c32edC70B295c744C43',
+      97: '',
+    },
+    decimals: 8,
+    projectLink: 'https://dogecoin.com/',
+  },
+
 }
 
 export default tokens
